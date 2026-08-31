ff6fd3d3fbde328051ae5ac24e2536518761b33a
dnday <marceldino9@gmail.com>
2026-08-31 14:39:08 +0700

    feat: add initial implementation of bubble sort and display functionality

---
+++ week2/Program.cs
@@ -0,0 +1,61 @@
+﻿// Console.WriteLine("Kamu siapa?");
+// string nama = Console.ReadLine() ?? string.Empty;
+// Console.WriteLine($"Halo {nama}");
+// Console.ReadLine();
+
+// try{
+// Console.WriteLine("Masukkan sebuah angka");
+// int angka = int.Parse(Console.ReadLine());
+// Console.WriteLine("Anda memasukkan : " + angka.ToString());
+// }
+// catch (Exception e)
+// {
+// Console.WriteLine("Error " + e.Message);
+// }
+// Console.ReadLine();
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] kumpulanAngka = [23, 1, 43, 323, 6, 3, 13, 54, 2];
+        TampilkanKumpulanAngka(kumpulanAngka);
+        int[] angkaTerurut = BubbleSort(kumpulanAngka);
+        TampilkanKumpulanAngka(angkaTerurut);
+        Console.ReadLine();
+    }
+
+    private static void TampilkanKumpulanAngka(int[] kumpulanAngka)
+    {
+        foreach (int angka in kumpulanAngka)
+        {
+            Console.WriteLine(angka);
+        }
+
+        Console.WriteLine();
+    }
+
+    private static void TukarTempatAngka(ref int angka1, ref int angka2)
+    {
+        int temp = angka1;
+        angka1 = angka2;
+        angka2 = temp;
+    }
+
+    private static int[] BubbleSort(int[] kumpulanAngka)
+    {
+        bool terurut = false;
+        while (!terurut)
+        {
+            terurut = true;
+            for (int i = 0; i < kumpulanAngka.Length - 1; i++)
+            {
+                if (kumpulanAngka[i] > kumpulanAngka[i + 1])
+                {
+                    TukarTempatAngka(ref kumpulanAngka[i], ref kumpulanAngka[i + 1]);
+                    terurut = false;
+                }
+            }
+        }
+        return kumpulanAngka;
+    }
+}
